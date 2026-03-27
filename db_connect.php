78749ce4eef3f13ca844078a618db63b0113a205
jus-montoya <justinesmontoya@gmail.com>
2026-03-27 14:00:16 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,19 @@
 <?php
 
-$servername = "db"; 
-$username = "root";
-$password = "root"; 
-$dbname = "tri2go_db";
+// Aiven Connection Details
+$servername = "mysql-add1e04-justinesmontoya06.j.aivencloud.com"; 
+$username   = "avnadmin";
+$password   = "AVNS_qY9L-L4Nhj_0nGfhN1y"; 
+$dbname     = "defaultdb"; 
+$port       = 12132;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection including the port number for cloud hosting
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// If you get here, your TRI2GO app is successfully connected!
 ?>
